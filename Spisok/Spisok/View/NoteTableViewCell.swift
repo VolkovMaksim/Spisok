4c15b7120fa65f89f0fd8064941e002345b28534
Maksim Volkov <volkov.md@bk.ru>
2023-06-29 09:10:28 +0300

    Create model and logic for cell

---
+++ Spisok/Spisok/View/NoteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoteTableViewCell.swift
+//  Spisok
+//
+//  Created by Maksim Volkov on 29.06.2023.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
